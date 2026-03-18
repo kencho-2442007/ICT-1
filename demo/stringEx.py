d7a8e9cb7493b3ee9f5433c44942ce7de997ca2c
User <08250271.sce@rub.edu.bt>
2026-03-18 09:58:28 +0600

    stringpythonpractice

---
+++ demo/stringEx.py
@@ -0,0 +1,12 @@
+print("Hello")
+print('Hello')
+printmultipleLines='''This is a string 
+that spans multiple lines'''
+print("multipleLines")
+
+a=1
+b=2
+c=a+b
+print(type(c)) #Data Type of c is int
+c=str(c) #Type Casting
+print(c) #Data Type of c is now str
