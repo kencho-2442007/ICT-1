2e947ef4d93a53e6bd509247cbb45cf3e3a536f9
User <08250271.sce@rub.edu.bt>
2026-03-20 16:12:28 +0600

    activity

---
+++ demo/studentactivity.py
@@ -0,0 +1,13 @@
+name=input("Enter your name: ")
+num1=float(input("Enter first number: "))
+num2=float(input("Enter second number: "))
+addition_result=num1+num2
+subtraction_result=num1-num2
+multiplication_result=num1*num2
+division_result=num1/num2
+print("Hello", name)
+print("Addition:", addition_result)
+print("Subtraction:", subtraction_result)
+print("Multiplication:", multiplication_result)
+print("Division:", division_result)
+
