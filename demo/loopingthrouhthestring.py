096b09d42385e03959940fafa9d98ebc8a2978cf
User <08250271.sce@rub.edu.bt>
2026-04-09 19:35:04 +0600

    learning loop

--- demo/loopingthrouhthestring.py
+++ demo/loopingthrouhthestring.py
@@ -10,3 +10,14 @@ lenli=len(li)
 for x in range(lenli):
     print(li[x])
 
+tupleli=tuple(li)
+for x in range(len(tupleli)):
+    print(tupleli[x])
+
+setli=set(li)
+for x in range(len(setli)):
+    print(x)
+    
+
+
+
