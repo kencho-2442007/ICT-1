e984b65b124dc1923be4e6f6f9b8e5d5e8d9ad3a
User <08250271.sce@rub.edu.bt>
2026-04-09 16:18:19 +0600

    looping through string

---
+++ demo/loopingthrouhthestring.py
@@ -0,0 +1,12 @@
+name=input("Enter your name: ")
+for i in name:
+    print(i)
+
+li=["Python Programming", "Python fundamentaks", "Python Interview Question"]
+for x in li:
+    print(x)
+
+lenli=len(li) 
+for x in range(lenli):
+    print(li[x])
+
