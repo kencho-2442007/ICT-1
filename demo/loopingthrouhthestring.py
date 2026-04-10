9e41defc29e1e82724935ec1496e45194d3e3513
User <08250271.sce@rub.edu.bt>
2026-04-10 15:29:40 +0600

    loop for set list and tuple

--- demo/loopingthrouhthestring.py
+++ demo/loopingthrouhthestring.py
@@ -18,6 +18,16 @@ setli=set(li)
 for x in range(len(setli)):
     print(x)
     
+tup=("John Smith","Jane Doe","Alice Johnson")
+for x in tup:
+    print(x)
 
+set1={10,20,30}
+for x in set1:
+    print(x)
+
+BookDetails=dict({"python programming": "john smith", "Python Fundamentals": "alice johnson", "python interview questions":"john doe"})
+for keys in BookDetails:
+    print(keys, BookDetails[keys])
 
 
