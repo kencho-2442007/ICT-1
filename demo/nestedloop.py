05ca4329c952b6ee454f821a0bead5453876c2ec
User <08250271.sce@rub.edu.bt>
2026-04-10 16:27:09 +0600

    learning nested loop

--- demo/nestedloop.py
+++ demo/nestedloop.py
@@ -1,3 +1,19 @@
 for i in range(1,4): 
     for j in range(i):
        print(f"Outer loop iteration {i}, inner loop iteration {j+1}")
+
+for i in range(4):
+    for j in range(i):
+        print("*", end= "")
+    print()
+
+for i in range(1,6):
+    for j in range(1,i+1):
+       print(j,end="")
+    print()
+
+for i in range(6,0,-1):
+    for j in range(i):
+        print("*", end=" ")
+    print()
+
