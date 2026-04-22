3ee862a2672e8a722907f3ae9d9696c78076e425
User <08250271.sce@rub.edu.bt>
2026-04-22 09:42:50 +0600

    parameters

---
+++ demo/parameter.py
@@ -0,0 +1,7 @@
+def sum_with_parameters(x,y):
+    print("the sum of",x, "and", y, "is:",x+y)
+sum_with_parameters(3,7)
+
+def product_with_parameters(x,y):
+    print("The product of ",x,"and",y,"is:",x*y)
+product_with_parameters(3,7)
