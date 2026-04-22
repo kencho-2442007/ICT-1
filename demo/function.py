7b1434b96b23dd237a73d205dcd8469441403d55
User <08250271.sce@rub.edu.bt>
2026-04-22 09:34:52 +0600

    function

---
+++ demo/function.py
@@ -0,0 +1,13 @@
+def sum():
+    a=5
+    b=10
+    print("The sum of a and b is:", a+b)
+
+def product():
+    a=5
+    b=10
+    print("The product of a and b is:", a*b)
+
+# sum()
+# product()
+    
