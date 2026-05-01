2b1ad3f789d4309da3f1f62871a885eb48a974fb
User <08250271.sce@rub.edu.bt>
2026-05-01 15:42:45 +0600

    lambda

---
+++ demo/lambda.py
@@ -0,0 +1,29 @@
+name=input("enter your name:")
+greet =lambda x: print("hello", x)
+greet(name)
+
+#condition checking
+even_odd= lambda x: "even" if x%2 ==0 else "odd"
+num= int(input("enter a number: "))
+print(even_odd(num))
+
+#return multiple result
+arith= lambda x, y: (x+y, x-y, x*y, x/y)
+num1= int(input("enter first number:"))
+num2= int(input("enter second number:"))
+print(arith(num1,num2))
+
+#filter
+mylist= [1,2,3,4,5,6]
+even= filter(lambda x: x%2 ==0, mylist)
+print(list(even))
+
+#map
+mylist= [1,2,3,4]
+double= map (lambda x: x*2, mylist)
+print(list(double))
+
+#convert result of double to my list
+double= map (lambda x: x*2, mylist)
+mylist= map (lambda x: x/2, double)
+print(list(mylist))
